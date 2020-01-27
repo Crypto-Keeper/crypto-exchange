0073538dd984b9f203f3102da4637439c43fe132
Abaas Khorrami <abaas.khorrami@gmail.com>
2020-01-27 11:02:22 -0500

    cleaning out repo

--- client/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import App from './App.jsx';
-
-// Hook in our REact to a div component in the HTML page
-
-render(<App />, document.getElementById('root'));
