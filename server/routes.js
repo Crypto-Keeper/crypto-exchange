37ff18efa42864910a0558f5f0ee61e511474bd2
William Chen <williamchen@Williams-MacBook-Air.local>
2020-01-27 11:31:50 -0500

    small name changes

--- server/routes.js
+++ server/routes.js
@@ -11,18 +11,18 @@ router.post('/login', cryptoController.login, cryptoController.getAsk, cryptoCon
 // router.get('/market', cryptoController.getMarket, (req, res) => res.status(200).json(res.locals.body));
 
 // add limit
-router.get('/limit', cryptoController.addLimit, cryptoController.getAsk, cryptoController.getBid, (req, res) => res.status(200).json(res.locals.body));
+router.get('/sellLimit', cryptoController.sellLimit, cryptoController.getAsk, cryptoController.getBid, (req, res) => res.status(200).json(res.locals.body));
 
 
 //need to handle post for update user
 // router.post('/login', cryptoController.addLogin, (req, res) => res.status(200).json());
 
 // need to handle post for update market
-router.post('/market', cryptoController.updateMarket, (req, res) => res.status(200).json());
+router.post('/buyMarket', cryptoController.buyMarket, (req, res) => res.status(200).json());
 
 
 // need to handle post for update limit
-router.post('/limit', cryptoController.updateLimit, (req, res) => res.status(200).json());
+router.post('/buyLimit', cryptoController.buyLimit, (req, res) => res.status(200).json());
 
 
 module.exports = router;
